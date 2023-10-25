ed5e92f8ad387ba10267aad4bfa274129602aa41
HSI Shital <hsishital@HSIs-MacBook-Pro.local>
2023-10-25 12:28:22 +0530

    KD-86: Paragraph slider twig files.

--- themes/kamihaya_digitalagency/js/components/slider.js
+++ themes/kamihaya_digitalagency/js/components/slider.js
@@ -1,6 +1,32 @@
 /**
-   * Init swiper sliders
+   * Easy selector helper function
    */
+const select = (el, all = false) => {
+  el = el.trim()
+  if (all) {
+    return [...document.querySelectorAll(el)]
+  } else {
+    return document.querySelector(el)
+  }
+}
+
+/**
+ * Easy event listener function
+ */
+const on = (type, el, listener, all = false) => {
+  let selectEl = select(el, all)
+  if (selectEl) {
+    if (all) {
+      selectEl.forEach(e => e.addEventListener(type, listener))
+    } else {
+      selectEl.addEventListener(type, listener)
+    }
+  }
+}
+
+/**
+ * Init swiper sliders
+ */
 function initSwiper() {
   document.querySelectorAll('.swiper').forEach(function(swiper) {
     let config = JSON.parse(swiper.querySelector('.swiper-config').innerHTML.trim());
