94b5ab2f331eedcb4277cd646c630be4b788b893
Masami Suzuki <masami@GEN-APN-026.local>
2024-09-09 18:58:54 +0900

    Change login page URL

--- modules/kamihaya_cms_auth/src/Routing/KamihayaRouteSubscriber.php
+++ modules/kamihaya_cms_auth/src/Routing/KamihayaRouteSubscriber.php
@@ -12,23 +12,6 @@ use Symfony\Component\Routing\RouteCollection;
 class KamihayaRouteSubscriber extends RouteSubscriberBase {
 
   /**
-   * The config factory.
-   *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
-   */
-  protected $configFactory;
-
-  /**
-   * Constructs a new AwsCloudRouteSubscriber object.
-   *
-   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   A configuration factory.
-   */
-  public function __construct(ConfigFactoryInterface $config_factory) {
-    $this->configFactory = $config_factory;
-  }
-
-  /**
    * {@inheritdoc}
    */
   protected function alterRoutes(RouteCollection $collection): void {
