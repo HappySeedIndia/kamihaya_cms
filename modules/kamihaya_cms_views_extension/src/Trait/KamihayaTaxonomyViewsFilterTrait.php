f3fdd8b8e73f2b3a31ee1f9499e3e32b4190301e
Masami Suzuki <masami@GEN-APN-026.local>
2024-12-22 16:47:42 +0900

    Improve exposed form

--- modules/kamihaya_cms_views_extension/src/Trait/KamihayaTaxonomyViewsFilterTrait.php
+++ modules/kamihaya_cms_views_extension/src/Trait/KamihayaTaxonomyViewsFilterTrait.php
@@ -129,51 +129,52 @@ trait KamihayaTaxonomyViewsFilterTrait {
         $filters[] = $request_params[$name];
       }
     }
-    $arguments = [];
-    $route_term = $this->routeMatch->getParameter('taxonomy_term');
-    foreach ($this->view->argument as $name => $argument) {
-      if (strpos($argument->options['plugin_id'], 'taxonomy') === FALSE || empty($argument->options['validate_options']['bundles'])) {
-        continue;
+    if (!empty($this->view->argument)) {
+      $arguments = [];
+      $route_term = $this->routeMatch->getParameter('taxonomy_term');
+      foreach ($this->view->argument as $name => $argument) {
+        if (strpos($argument->options['plugin_id'], 'taxonomy') === FALSE || empty($argument->options['validate_options']['bundles'])) {
+          continue;
+        }
+        if (empty($argument->options['default_argument_type'] || ($argument->options['default_argument_type'] !== 'taxonomy_term') && $argument->options['default_argument_type'] !== 'taxonomy_tid')) {
+          continue;
+        }
+        $idx = array_search($name, array_keys($this->view->argument));
+        if (!empty($this->view->args[$idx])) {
+          $arguments[] = $this->view->args[$idx];
+          continue;
+        }
+        if (!empty($route_term) && in_array($route_term->bundle(), $argument->options['validate_options']['bundles']) && !in_array($route_term->id(), $arguments)) {
+          $arguments[] = $route_term->id();
+        }
       }
-      if (empty($argument->options['default_argument_type'] || ($argument->options['default_argument_type'] !== 'taxonomy_term') && $argument->options['default_argument_type'] !== 'taxonomy_tid')) {
-        continue;
-      }
-      $idx = array_search($name, array_keys($this->view->argument));
-      if (!empty($this->view->args[$idx])) {
-        $arguments[] = $this->view->args[$idx];
-        continue;
-      }
-      if (!empty($route_term) && in_array($route_term->bundle(), $argument->options['validate_options']['bundles']) && !in_array($route_term->id(), $arguments)) {
-        $arguments[] = $route_term->id();
+
+      foreach ($arguments as $tid) {
+        $arg_term = Term::load($tid);
+        if (empty($arg_term)) {
+          continue;
+        }
+        if (in_array($arg_term->bundle(), $bundles)) {
+          $filters[] = $tid;
+          continue;
+        }
+        $field_definitions = $this->entityFieldManager->getFieldDefinitions('taxonomy_term', $arg_term->bundle());
+        foreach ($field_definitions as $field_name => $field_definition) {
+          if (strpos($field_name, 'field_') !== 0 || $field_definition->getType() !== 'entity_reference' || strpos($field_definition->getSetting('handler'), 'taxonomy_term') === FALSE) {
+            continue;
+          }
+          $target_bundle = reset($field_definition->getSetting('handler_settings')['target_bundles']);
+          if (!in_array($target_bundle, $bundles)) {
+            continue;
+          }
+          $field_value = $arg_term->get($field_name)->getValue();
+          if (empty($field_value[0]['target_id'])) {
+            continue;
+          }
+          $filters[] = $field_value[0]['target_id'];
+        }
       }
     }
-
-    foreach ($arguments as $tid) {
-      $arg_term = Term::load($tid);
-      if (empty($arg_term)) {
-        continue;
-      }
-      if (in_array($arg_term->bundle(), $bundles)) {
-        $filters[] = $tid;
-        continue;
-      }
-      $field_definitions = $this->entityFieldManager->getFieldDefinitions('taxonomy_term', $arg_term->bundle());
-      foreach ($field_definitions as $field_name => $field_definition) {
-        if (strpos($field_name, 'field_') !== 0 || $field_definition->getType() !== 'entity_reference' || strpos($field_definition->getSetting('handler'), 'taxonomy_term') === FALSE) {
-          continue;
-        }
-        $target_bundle = reset($field_definition->getSetting('handler_settings')['target_bundles']);
-        if (!in_array($target_bundle, $bundles)) {
-          continue;
-        }
-        $field_value = $arg_term->get($field_name)->getValue();
-        if (empty($field_value[0]['target_id'])) {
-          continue;
-        }
-        $filters[] = $field_value[0]['target_id'];
-      }
-    }
-
     if (empty($filters)) {
       $form['value']['#options'] = [];
       return;
