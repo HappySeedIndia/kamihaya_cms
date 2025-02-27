f51a68c26bf1f639a3fd08e961bdae7a4a81a4d2
Masami Suzuki <masami@GEN-APN-026.local>
2025-02-27 16:44:31 +0900

    Fix the typo of class name

--- modules/kamihaya_cms_views_extension/src/Plugin/views/exposed_form/KamihayaExposedForm.php
+++ modules/kamihaya_cms_views_extension/src/Plugin/views/exposed_form/KamihayaExposedForm.php
@@ -9,7 +9,6 @@ use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\better_exposed_filters\Plugin\BetterExposedFiltersWidgetManager;
 use Drupal\better_exposed_filters\Plugin\views\exposed_form\BetterExposedFilters;
-use Drupal\Core\Theme\ThemeManagerInterface;
 use Drupal\views\Attribute\ViewsExposedForm;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -23,10 +22,10 @@ use Symfony\Component\HttpFoundation\Request;
   id: 'kamihaya_exposed_form',
   title: new TranslatableMarkup('Kamihaya Exposed Form'),
 )]
-class KamihahyaExposedForm extends BetterExposedFilters {
+class KamihayaExposedForm extends BetterExposedFilters {
 
-    /**
-   * KamihahyaExposedForm constructor.
+  /**
+   * KamihayaExposedForm constructor.
    *
    * @param array $configuration
    *   A configuration array containing information about the plugin instance.
@@ -42,12 +41,13 @@ class KamihahyaExposedForm extends BetterExposedFilters {
    *   The better exposed filter widget manager for sort widgets.
    * @param \Drupal\Core\Extension\ModuleHandlerInterface $moduleHandler
    *   Manage drupal modules.
-   * @param \Drupal\Core\Theme\ThemeManagerInterface $themeManager
-   *   Manage drupal themes.
    * @param \Drupal\Core\Render\ElementInfoManagerInterface $elementInfo
    *   The element info manager.
    * @param \Symfony\Component\HttpFoundation\Request $request
    *   The Request object.
+   *
+   * @patam \Drupal\Core\Routing\RouteMatchInterface
+   *  The currently active route match object.
    */
   public function __construct(
     array $configuration,
@@ -57,12 +57,11 @@ class KamihahyaExposedForm extends BetterExposedFilters {
     protected BetterExposedFiltersWidgetManager $pagerWidgetManager,
     protected BetterExposedFiltersWidgetManager $sortWidgetManager,
     protected ModuleHandlerInterface $moduleHandler,
-    protected ThemeManagerInterface $themeManager,
     protected ElementInfoManagerInterface $elementInfo,
     protected Request $request,
     protected RouteMatchInterface $routeMatch,
   ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $filterWidgetManager, $pagerWidgetManager, $sortWidgetManager, $moduleHandler, $themeManager, $elementInfo, $request);
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $filterWidgetManager, $pagerWidgetManager, $sortWidgetManager, $moduleHandler, $elementInfo, $request);
   }
 
   /**
@@ -78,7 +77,6 @@ class KamihahyaExposedForm extends BetterExposedFilters {
       $container->get('plugin.manager.better_exposed_filters_pager_widget'),
       $container->get('plugin.manager.better_exposed_filters_sort_widget'),
       $container->get('module_handler'),
-      $container->get('theme.manager'),
       $container->get('element_info'),
       $container->get('request_stack')->getCurrentRequest(),
       $container->get('current_route_match')
@@ -263,11 +261,9 @@ class KamihahyaExposedForm extends BetterExposedFilters {
       if (empty($form['actions']['submit']['#attributes']['class'])) {
         $form['actions']['submit']['#attributes']['class'] = [];
       }
-      if (empty($this->options['bef']['general']['autosubmit_hide'])) {
-        $form['actions']['submit']['#attributes']['class'][] = 'submit-icon';
-        $form['actions']['submit']['#prefix'] = '<div class="button-wrapper btn-primary">';
-        $form['actions']['submit']['#suffix'] = '</div>';
-      }
+      $form['actions']['submit']['#attributes']['class'][] = 'submit-icon';
+      $form['actions']['submit']['#prefix'] = '<div class="button-wrapper btn-primary">';
+      $form['actions']['submit']['#suffix'] = '</div>';
       if (!empty($this->options['bef']['general']['autosubmit_exclude_textfield'])
         && !empty($this->options['bef']['general']['autosubmit_exclude_textfield'])) {
         $text_fields = [];
