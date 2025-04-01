9eb363b8417d4254e17160dd2c108447e4c9cfc3
hs-gautam <gautam@genero.jp>
2025-04-01 14:14:27 +0530

    KD-551: Initial implementation for estimate calculator.

---
+++ modules/kamihaya_cms_views_extension/js/estimate.js
@@ -0,0 +1,40 @@
+(function ($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.estimateCalculator = {
+    attach: function (context, settings) {
+      function calculateTotal() {
+        var total = 0;
+
+        $('.views-field-kamihaya-cms-views-extension-estimate p[data-price]', context).each(function () {
+          var price = parseFloat($(this).attr('data-total-price')) || 0;
+          total += price;
+        });
+        $('.view-footer p .estimate-total').text(total.toFixed(2)); // Update total in a specific element
+      }
+
+      // Call initially on page load
+      calculateTotal();
+
+      $('.views-field-kamihaya-cms-views-extension-quantity .js-kamihaya-cms-quantity', context).each(function () {
+        $(this).on('change', function () {
+          const quantity = parseInt($(this).val()) || 0;
+          // Get the price from the estimate field
+          const priceField = $(this)
+            .closest('.views-field-kamihaya-cms-views-extension-quantity') // Find the closest parent field
+            .siblings('.views-field-kamihaya-cms-views-extension-estimate') // Find the next sibling field
+            .find('p'); // Get the <p> element inside it
+
+          const price = parseFloat(priceField.data('price')) || 0; // Ensure trimmed text
+          // Calculate total
+          const total = price * quantity;
+          if (priceField.length) {
+            priceField.text(total);
+            priceField.attr('data-total-price', total);
+            calculateTotal();
+          }
+        });
+      });
+    }
+  };
+})(jQuery, Drupal);
